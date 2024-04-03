7e84279411f0062b3498eca70d8355ab7799704e
vishwajitmalusare <you@example.com>
2024-04-04 03:26:05 +0530

    Add. like avatar save movie functionlity

--- src/Pages/moviedetails.js
+++ src/Pages/moviedetails.js
@@ -1,5 +1,7 @@
 import {
+  ArrowBack,
   ExpandMore,
+  Favorite,
   FavoriteBorderOutlined,
   MoreVertOutlined,
   Share,
@@ -15,97 +17,164 @@ import {
   IconButton,
   Typography,
 } from "@mui/material";
-import React from "react";
-import { useLocation } from "react-router-dom";
+import React, { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import moviedata from "../Utility/moviedata.json";
-import Header from '../Components/header'
+import Header from "../Components/header";
 
 export default function MovieDetails() {
-const location = useLocation();
-const movieId = location.state.movieid;
-  // get navigate states  --> {location.state.name}
+  const location = useLocation();
+  const movieId = location.state.movieid;
+  const [liked, setLiked] = useState(false);
+  const navigate = useNavigate();
 
-  const movie = moviedata.movies.find(movie => movie.id === movieId);
-  console.log('movie: ', movie);
+  //Movie Data
+  const movie = moviedata.movies.find((movie) => movie.id === movieId);
+  console.log("movie: ", movie);
+
+  //Method for like
+  const toggleLike = () => {
+    setLiked(!liked);
+  };
+
+  //Method for handle back click
+  const handleBack = () => {
+    navigate("/");
+  };
+
+  //Method for Avatar Color
+  const getAvatarBackgroundColor = (initial) => {
+    const colors = {
+      A: "#f44336",
+      B: "#e91e63",
+      C: "#9c27b0",
+      D: "#673ab7",
+      E: "#3f51b5",
+      F: "#2196f3",
+      G: "#03a9f4",
+      H: "#00bcd4",
+      I: "#009688",
+      J: "#4caf50",
+      K: "#8bc34a",
+      L: "#cddc39",
+      M: "#ffeb3b",
+      N: "#ffc107",
+      O: "#ff9800",
+      P: "#ff5722",
+      Q: "#795548",
+      R: "#9e9e9e",
+      S: "#607d8b",
+      T: "#333333",
+      U: "#666666",
+      V: "#999999",
+      W: "#cccccc",
+      X: "#dddddd",
+      Y: "#bbbbbb",
+      Z: "#aaaaaa",
+    };
+    const defaultColor = "#2196f3"; // Default color if no match is found
+
+    const firstLetter = initial.charAt(0).toUpperCase();
+    return colors[firstLetter] || defaultColor;
+  };
 
   return (
     <>
-    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
-      <h2 >Movie Details</h2>
-      <Card sx={{ height: "500px", width: "70%",  marginLeft: 7, marginTop: 4 }}>
-        <CardHeader
-        sx={{ textAlign: 'center' }}
-          avatar={
-            <Avatar sx={{ bgcolor: "red[500]", justifyContent: 'center', alignItems: 'center'}} aria-label="recipe">
-              R
-            </Avatar>
-          }
-          action={
-            <IconButton aria-label="settings">
-              <MoreVertOutlined />
-            </IconButton>
-          }
-          title={movie.title}
-          subheader={movie.year}
-        />
-        <CardMedia
-          component="img"
-          height="160"
-          image={movie?.posterUrl}
-          alt={movie?.title}
-        />
-        <CardContent>
-        <Typography variant="body2" color="text.secondary">
-            Director :  {movie?.director}.
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Actor :  {movie?.actors}.
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-          Genres :  {movie?.genres}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-          Runtime :  {movie?.runtime}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Plot :  {movie?.plot}.
-          </Typography>
-        </CardContent>
-        <CardActions 
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <h2>Movie Details</h2>
+        <Card
+          raised
           sx={{
-            display: 'flex',
-            // justifyContent: 'center',
-            alignItems: 'flex-end',
-            marginTop: 'auto', // This will push the CardActions to the bottom
+            maxWidth: "70%",
+            margin: "0 auto",
+            padding: "0.1em",
           }}
         >
-          <IconButton aria-label="add to favorites">
-            <FavoriteBorderOutlined />
-          </IconButton>
-          <IconButton aria-label="share">
-            <Share />
-          </IconButton>
-          {/* <IconButton
-            onClick={() => handleExpandClick(index)}
-            aria-expanded={expanded[index]}
-            aria-label="show more"
-          >
-            <ExpandMore />
-          </IconButton> */}
-        </CardActions>
-        {/* <Collapse in={expanded[index]} timeout="auto" unmountOnExit>
+          <CardHeader
+            sx={{ textAlign: "center" }}
+            avatar={
+              <Avatar
+                sx={{
+                  bgcolor: getAvatarBackgroundColor(movie.title),
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+                aria-label="recipe"
+              >
+                {movie?.title ? movie?.title.charAt(0).toUpperCase() : "M"}
+              </Avatar>
+            }
+            // action={
+            //   <IconButton aria-label="settings">
+            //     <MoreVertOutlined />
+            //   </IconButton>
+            // }
+            action={
+              <IconButton aria-label="back" onClick={handleBack}>
+                <ArrowBack />
+              </IconButton>
+            }
+            title={movie.title}
+            subheader={movie.year}
+          />
+          <CardMedia
+            component="img"
+            height="250"
+            image={movie?.posterUrl}
+            alt={"alt"}
+            title={movie?.title}
+            sx={{ padding: "1em 1em 0 1em", objectFit: "contain" }}
+          />
           <CardContent>
-            <Typography paragraph>Additional Movie Details:</Typography>
-            {/* Map through movie details and render them /*}
-            {Object.entries(movie).map(([key, value]) => (
-              <Typography key={key} paragraph>
-                <strong>{key}:</strong> {value}
-              </Typography>
-            ))}
+            <Typography variant="body2" color="text.secondary">
+              Director : {movie?.director}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Actor : {movie?.actors}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Genres:
+              {movie.genres.map((genre, index) => (
+                <React.Fragment key={index}>
+                  {genre}
+                  {index < movie.genres.length - 1 && ", "}
+                </React.Fragment>
+              ))}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Runtime : {movie?.runtime}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Plot : {movie?.plot}
+            </Typography>
           </CardContent>
-        </Collapse> */}
-      </Card>
-      </div>    
-  </>
+          <CardActions
+            sx={{
+              display: "flex",
+              alignItems: "flex-end",
+              marginTop: "auto", // This will push the CardActions to the bottom
+            }}
+          >
+            <IconButton aria-label="add to favorites" onClick={toggleLike}>
+              {liked ? (
+                <Favorite style={{ color: "red" }} />
+              ) : (
+                <FavoriteBorderOutlined />
+              )}
+            </IconButton>
+            <IconButton aria-label="share">
+              <Share />
+            </IconButton>
+          </CardActions>
+        </Card>
+      </div>
+    </>
   );
 }
--- src/Pages/movies.js
+++ src/Pages/movies.js
@@ -1,83 +1,171 @@
 import React, { useState } from "react";
 import moviedata from "../Utility/moviedata.json";
-import { Avatar, Card, CardHeader, Collapse, IconButton, Typography } from "@mui/material";
-import { ExpandMore, FavoriteBorderOutlined, MoreVertOutlined, Share } from "@mui/icons-material";
+import {
+  Avatar,
+  Card,
+  CardHeader,
+  Collapse,
+  IconButton,
+  Typography,
+} from "@mui/material";
+import {
+  Bookmark,
+  BookmarkAdd,
+  BookmarkAdded,
+  ExpandMore,
+  Favorite,
+  FavoriteBorderOutlined,
+  InfoSharp,
+  InfoTwoTone,
+  MoreVertOutlined,
+  Share,
+} from "@mui/icons-material";
 import { CardActions, CardContent, CardMedia } from "@mui/material";
-import Header from '../Components/header'
+import Header from "../Components/header";
 import { useNavigate } from "react-router-dom";
 
 export default function Movies() {
   const [expanded, setExpanded] = useState({});
   const navigate = useNavigate();
-  const handleExpandClick = (movieId) => {
-    setExpanded((prevState) => ({
-      ...prevState,
-      [movieId]: !prevState[movieId],
-    }));
+  const [liked, setLiked] = useState(false);
+  const [savedMovies, setSavedMovies] = useState([]);
+
+  //Method for like
+  const toggleLike = () => {
+    setLiked(!liked);
   };
 
-  //Navigation with data sharing
-  //navigate("/new-route", { state: { key: "value" } });
+  //Method for Avatar Color
+  const getAvatarBackgroundColor = (initial) => {
+    const colors = {
+      A: "#f44336",
+      B: "#e91e63",
+      C: "#9c27b0",
+      D: "#673ab7",
+      E: "#3f51b5",
+      F: "#2196f3",
+      G: "#03a9f4",
+      H: "#00bcd4",
+      I: "#009688",
+      J: "#4caf50",
+      K: "#8bc34a",
+      L: "#cddc39",
+      M: "#ffeb3b",
+      N: "#ffc107",
+      O: "#ff9800",
+      P: "#ff5722",
+      Q: "#795548",
+      R: "#9e9e9e",
+      S: "#607d8b",
+      T: "#333333",
+      U: "#666666",
+      V: "#999999",
+      W: "#cccccc",
+      X: "#dddddd",
+      Y: "#bbbbbb",
+      Z: "#aaaaaa",
+      // Add more colors for other letters if needed
+    };
+    const defaultColor = "#2196f3"; // Default color if no match is found
+
+    const firstLetter = initial.charAt(0).toUpperCase();
+    return colors[firstLetter] || defaultColor;
+  };
+
+// Method to handle saving or unsaving a movie
+const handleSave = (movie) => {
+  if (isMovieSaved(movie)) {
+    // If the movie is already saved, remove it from savedMovies
+    setSavedMovies(savedMovies.filter(savedMovie => savedMovie.id !== movie.id));
+  } else {
+    // If the movie is not saved, add it to savedMovies
+    setSavedMovies([...savedMovies, movie]);
+  }
+};
+
+  // Method to check if a movie is saved
+  const isMovieSaved = (movie) => {
+    return savedMovies.some((savedMovie) => savedMovie.id === movie.id);
+  };
 
   return (
     <>
-    <Header />
-    <div style={{ display: 'flex', flexWrap: 'wrap', gap: '20px' }}>
-      {moviedata.movies.map((movie, index) => (
-        <Card key={index} sx={{ width: 250, marginLeft: 7, marginTop: 4 }} onClick={() => navigate("/moviedetails", { state: {movieid: movie.id}})}>
-          <CardHeader
-            avatar={
-              <Avatar sx={{ bgcolor: 'red[500]' }} aria-label="recipe">
-                R
-              </Avatar>
-            }
-            action={
-              <IconButton aria-label="settings">
-                <MoreVertOutlined />
-              </IconButton>
-            }
-            title={movie.title}
-            subheader={movie.release_date}
-          />
-          <CardMedia
-            component="img"
-            height="160"
-            image={movie.posterUrl}
-            alt={movie.title}
-          />
-          <CardContent>
-            <Typography variant="body2" color="text.secondary">
-              {movie.overview}
-            </Typography>
-          </CardContent>
-          <CardActions disableSpacing>
-            <IconButton aria-label="add to favorites">
-              <FavoriteBorderOutlined />
-            </IconButton>
-            <IconButton aria-label="share">
-              <Share />
-            </IconButton>
-            <IconButton
-              onClick={() => handleExpandClick(index)}
-              aria-expanded={expanded[index]}
-              aria-label="show more"
-            >
-              <ExpandMore />
-            </IconButton>
-          </CardActions>
-          <Collapse in={expanded[index]} timeout="auto" unmountOnExit>
+      <Header />
+      <div style={{ display: "flex", flexWrap: "wrap", gap: "20px" }}>
+        {moviedata.movies.map((movie, index) => (
+          <Card key={index} sx={{ width: 250, marginLeft: 7, marginTop: 4 }}>
+            <CardHeader
+              avatar={
+                <Avatar
+                  sx={{
+                    bgcolor: getAvatarBackgroundColor(movie.title),
+                    // bgcolor: "red", // Set your desired background color here
+                    color: "white", // Set text color to contrast with background
+                    width: 48, // Adjust size as needed
+                    height: 48, // Adjust size as needed
+                    fontSize: 24, // Adjust font size as needed
+                  }}
+                  aria-label="recipe"
+                >
+                  {movie?.title ? movie?.title.charAt(0).toUpperCase() : "M"}
+                </Avatar>
+                // <Avatar sx={{ bgcolor: 'red[500]' }} aria-label="recipe">
+                //   {movie?.title ? movie?.title.charAt(0).toUpperCase() : 'M'}
+                // </Avatar>
+              }
+              action={
+                <IconButton aria-label="settings">
+                  <MoreVertOutlined />
+                </IconButton>
+              }
+              title={movie.title}
+              subheader={movie.release_date}
+            />
+            <CardMedia
+              component="img"
+              height="160"
+              image={movie.posterUrl}
+              alt={movie.title}
+            />
             <CardContent>
-              <Typography paragraph>Additional Movie Details:</Typography>
-              {/* Map through movie details and render them */}
-              {Object.entries(movie).map(([key, value]) => (
-                <Typography key={key} paragraph>
-                  <strong>{key}:</strong> {value}
-                </Typography>
-              ))}
+              <Typography variant="body2" color="text.secondary">
+                {movie.overview}
+              </Typography>
             </CardContent>
-          </Collapse>
-        </Card>
-      ))}
+            <CardActions disableSpacing>
+      
+              <IconButton aria-label="share">
+                <Share />
+              </IconButton>
+              <IconButton
+                onClick={() =>
+                  navigate("/moviedetails", { state: { movieid: movie.id } })
+                }
+                aria-label=" show details"
+              >
+                <InfoTwoTone />
+              </IconButton>
+              <IconButton aria-label="save" onClick={() => handleSave(movie)}>
+                {isMovieSaved(movie) ? (
+                  <BookmarkAdded color="primary" />
+                ) : (
+                  <BookmarkAdd />
+                )}
+              </IconButton>
+            </CardActions>
+            <Collapse in={expanded[index]} timeout="auto" unmountOnExit>
+              <CardContent>
+                <Typography paragraph>Additional Movie Details:</Typography>
+                {/* Map through movie details and render them */}
+                {Object.entries(movie).map(([key, value]) => (
+                  <Typography key={key} paragraph>
+                    <strong>{key}:</strong> {value}
+                  </Typography>
+                ))}
+              </CardContent>
+            </Collapse>
+          </Card>
+        ))}
       </div>
     </>
   );
